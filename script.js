bb464a48c03d534fe59745528aa7bce2504ee7b6
Rabbi <140103650+rabbi6272@users.noreply.github.com>
2024-05-26 09:12:19 +0600

    bug fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 let city = document.getElementById("city-name");
 let country = document.getElementById("country");
@@ -14,75 +14,76 @@ let dateTime = document.getElementById("date-time");
 
 let now = new Date();
 const date = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "medium",  
-    timeStyle: "short",
+  dateStyle: "medium",
+  timeStyle: "short",
 }).format(now);
 
-function implementData(data){
-    document.querySelector('input').value='';
-    dateTime.textContent = date;
-    city.textContent= data.name;
-    country.textContent= data.sys.country;
-    temp.textContent= Math.trunc(data.main.temp);
-    description.textContent= data.weather[0].description;
-    feelsLike.textContent= Math.trunc(data.main.feels_like);
-    min.textContent= Math.trunc(data.main.temp_min);
-    max.textContent=  Math.trunc(data.main.temp_max);
-    humidity.textContent= data.main.humidity;
-    wind.textContent= data.wind.speed;
-    let src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    img.src= src;
-    document.querySelector('.responce').style.opacity = 1;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      getWeatherByChords(latitude, longitude);
+    },
+    () => {
+      console.log("Could not get location");
+    }
+  );
 }
 
 async function getWeather() {
-    let city = document.getElementById("city").value;
-    if(city){
-        let url = `https://api.openweathermap.org/data/2.5/weather?appid=bbe7d4cf002908df2b1cf82e3d4a947e&units=metric&q=${city}`;
-        let res = await fetch(url);
-        console.log(res);
-        if(res.status == 200){
-            let data = await res.json();
-            implementData(data);
-        }else{
-            invalidCity();
-        }
-    } else{
-        invalidCity();
+  let city = document.getElementById("city").value;
+  if (city) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?appid=bbe7d4cf002908df2b1cf82e3d4a947e&units=metric&q=${city}`;
+    let res = await fetch(url);
+    if (res.status == 200) {
+      let data = await res.json();
+      implementData(data);
+    } else {
+      invalidCity();
     }
+  } else {
+    invalidCity();
+  }
 }
 
-async function getWeatherByChords(latitude, longitude){
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude.toFixed(4)}&lon=${longitude.toFixed(4)}&appid=bbe7d4cf002908df2b1cf82e3d4a947e&units=metric`;
-    let res = await fetch(url);
-    if(res.status == 200){
-        let data = await res.json();
-        implementData(data);
-    }
-};
-
-if(navigator.geolocation){
-navigator.geolocation.getCurrentPosition((position)=>{
-    const {latitude} = position.coords;
-    const {longitude} = position.coords;
-    getWeatherByChords(latitude, longitude)
-}, ()=>{
-    console.log('Could not get location');
-})
+async function getWeatherByChords(latitude, longitude) {
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude.toFixed(
+    4
+  )}&lon=${longitude.toFixed(
+    4
+  )}&appid=bbe7d4cf002908df2b1cf82e3d4a947e&units=metric`;
+  let res = await fetch(url);
+  if (res.status == 200) {
+    let data = await res.json();
+    implementData(data);
+  }
 }
 
 function invalidCity() {
-    let input = document.querySelector('input');
-    input.classList.add('invalid-city');
-    input.value='';
-    setTimeout(()=>{
-        input.classList.remove('invalid-city');
-    }, 3000)
+  let input = document.querySelector("input");
+  input.classList.add("invalid-city");
+  input.value = "";
+  setTimeout(() => {
+    input.classList.remove("invalid-city");
+  }, 3000);
 }
 
+function implementData(data) {
+  document.querySelector("input").value = "";
+  dateTime.textContent = date;
+  city.textContent = data.name;
+  country.textContent = data.sys.country;
+  temp.textContent = Math.trunc(data.main.temp);
+  description.textContent = data.weather[0].description;
+  feelsLike.textContent = Math.trunc(data.main.feels_like);
+  min.textContent = Math.trunc(data.main.temp_min);
+  max.textContent = Math.trunc(data.main.temp_max);
+  humidity.textContent = data.main.humidity;
+  wind.textContent = data.wind.speed;
+  let src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  img.src = src;
+  document.querySelector(".responce").style.opacity = 1;
+}
 
 document.querySelector("#submit").addEventListener("click", getWeather);
-
-
-
-
